38bdcec6fc7eb4244e27399e7105f83c31bd66de
Nathan Xie <nathanxie@nathans-air-2.3521.dhcp.asu.edu>
2023-10-08 09:19:43 -0700

    renamed jsonObject and betterJsonObject

--- backend/parse.js
+++ backend/parse.js
@@ -9,43 +9,43 @@ const parser = new XMLParser();
 const xmlFiles = fs.readdirSync(parseDir)
                     .filter(file => file.endsWith('.xml'));
 
-let betterJsonObjects = [];
+let filteredJsonTaxForm = [];
 
 xmlFiles.forEach(file => {
     let data = fs.readFileSync(`${parseDir}/${file}`, 'utf-8');
-    let jsonObj = parser.parse(data).Return;
+    let jsonTaxForm = parser.parse(data).Return;
 
-    let betterJsonObj = {
-        ein: String(jsonObj.ReturnHeader.PreparerFirmGrp.PreparerFirmEIN),
-        returnType: String(jsonObj.ReturnHeader.ReturnTypeCd),
-        city: jsonObj.ReturnHeader.PreparerFirmGrp.PreparerUSAddress.CityNm,
-        state: jsonObj.ReturnHeader.PreparerFirmGrp.PreparerUSAddress.StateAbbreviationCd
+    let filteredJsonTaxForm = {
+        ein: String(jsonTaxForm.ReturnHeader.PreparerFirmGrp.PreparerFirmEIN),
+        returnType: String(jsonTaxForm.ReturnHeader.ReturnTypeCd),
+        city: jsonTaxForm.ReturnHeader.PreparerFirmGrp.PreparerUSAddress.CityNm,
+        state: jsonTaxForm.ReturnHeader.PreparerFirmGrp.PreparerUSAddress.StateAbbreviationCd
     };
 
-    switch(betterJsonObj.returnType) {
+    switch(filteredJsonTaxForm.returnType) {
         case "990":
-            betterJsonObj.grossRevenue = jsonObj.ReturnData.IRS990.CYTotalRevenueAmt;
-            betterJsonObj.netRevenue = jsonObj.ReturnData.IRS990.CYRevenuesLessExpensesAmt
-            betterJsonObj.employees = jsonObj.ReturnData.IRS990.Form990PartVIISectionAGrp
+            filteredJsonTaxForm.grossRevenue = jsonTaxForm.ReturnData.IRS990.CYTotalRevenueAmt;
+            filteredJsonTaxForm.netRevenue = jsonTaxForm.ReturnData.IRS990.CYRevenuesLessExpensesAmt
+            filteredJsonTaxForm.employees = jsonTaxForm.ReturnData.IRS990.Form990PartVIISectionAGrp
             break;
         case "990EZ":
-            betterJsonObj.grossRevenue = jsonObj.ReturnData.IRS990EZ.TotalRevenueAmt
-            //betterJsonObj.grossRevenue = jsonObj.ReturnData.IRS990EZ.AnalysisOfRevenueAndExpenses.TotalExpensesRevAndExpnssAmt;
-            betterJsonObj.netRevenue = jsonObj.ReturnData.IRS990EZ.ExcessOrDeficitForYearAmt
-            //betterJsonObj.netRevenue = jsonObj.ReturnData.IRS990EZ.AnalysisOfRevenueAndExpenses.NetInvestmentIncomeAmt;
-            betterJsonObj.employees = jsonObj.ReturnData.IRS990EZ.OfficerDirectorTrusteeEmplGrp;
+            filteredJsonTaxForm.grossRevenue = jsonTaxForm.ReturnData.IRS990EZ.TotalRevenueAmt
+            //filteredJsonTaxForm.grossRevenue = jsonTaxForm.ReturnData.IRS990EZ.AnalysisOfRevenueAndExpenses.TotalExpensesRevAndExpnssAmt;
+            filteredJsonTaxForm.netRevenue = jsonTaxForm.ReturnData.IRS990EZ.ExcessOrDeficitForYearAmt
+            //filteredJsonTaxForm.netRevenue = jsonTaxForm.ReturnData.IRS990EZ.AnalysisOfRevenueAndExpenses.NetInvestmentIncomeAmt;
+            filteredJsonTaxForm.employees = jsonTaxForm.ReturnData.IRS990EZ.OfficerDirectorTrusteeEmplGrp;
             break;
         case "990PF":
-            betterJsonObj.onlyContributeToPreselected = jsonObj.ReturnData.IRS990PF.SupplementaryInformationGrp.OnlyContriToPreselectedInd
+            filteredJsonTaxForm.onlyContributeToPreselected = jsonTaxForm.ReturnData.IRS990PF.SupplementaryInformationGrp.OnlyContriToPreselectedInd
             // includes RecipientFoundationStatusTxt, GrantOrContributionPurposeTxt, Amt
-            betterJsonObj.contributions = jsonObj.ReturnData.IRS990PF.SupplementaryInformationGrp.GrantOrContributionPdDurYrGrp
-            betterJsonObj.employees = [];
+            filteredJsonTaxForm.contributions = jsonTaxForm.ReturnData.IRS990PF.SupplementaryInformationGrp.GrantOrContributionPdDurYrGrp
+            filteredJsonTaxForm.employees = [];
             break;
         default:
     }
 
     //Filter out employees with titles that aren't used in search terms
-    betterJsonObj.employees = betterJsonObj.employees.filter(o => {
+    filteredJsonTaxForm.employees = filteredJsonTaxForm.employees.filter(o => {
         let title = o.TitleTxt.toLowerCase();
         return title.includes("director") ||
                 (title.includes("chief") && title.includes("officer")) ||
@@ -53,7 +53,10 @@ xmlFiles.forEach(file => {
                 title.includes("coordinator") ||
                 title.includes("assistant")
     });
-    betterJsonObjects.push(betterJsonObj);
+    filteredJsonTaxForm.push(filteredJsonTaxForm);
 })
-
-send(betterJsonObjects);
+if(filteredJsonTaxForm.returnType === '990' ||
+filteredJsonTaxForm.returnType === '990EZ' || 
+filteredJsonTaxForm.returnType === '990PF'){
+    send(filteredJsonTaxForm);
+}
